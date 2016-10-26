ab78b92e1830d220167d215ce6e5853fc9dbd7e7
Jesse Thomson <jessethomson11@gmail.com>
2016-10-25 22:42:40 -0600

    Everything

---
+++ src/client/js/app.js
@@ -0,0 +1,113 @@
+var app = angular.module("myApp", []);
+
+app.controller("chatCtrl", ["$scope", "$http", "socket", function($scope, $http, socket) {
+
+	$scope.converstions = {};
+	$scope.users = [];
+
+	// load user
+	$http.get("/users").then(function(response) {
+		var users = response.data;
+		for(var i=0; i<users.length; i++) {
+			$scope.users.push(users[i]);
+			$scope.converstions[users[i].socketId] = [];
+		}
+		//$scope.users = response.data;
+	}, function(err) {
+		console.log(err);
+	});
+
+	$scope.registerUser = function(username) {
+		console.log("username: " + username)
+		$scope.myName = username;
+		socket.emit("new user", $scope.myName);
+	}
+	//createUser(Math.random());
+
+	$scope.onSend = function() {
+		if($scope.message) {	
+			socket.emit("chat message", {
+				from: $scope.me.socketId,
+				msg: $scope.message,
+				to: $scope.selectedFriend.socketId
+			});
+			$scope.message = "";
+			var objDiv = document.getElementById("chat-box");
+			objDiv.scrollTop = objDiv.scrollHeight;
+		}
+	}
+
+	$scope.selectFriend = function(friend) {
+		$scope.selectedFriend = friend;
+		if(!$scope.converstions[friend.socketId]) {
+			$scope.converstions[friend.socketId] = [];
+		}
+		$scope.currentConversation = $scope.converstions[friend.socketId];
+	}
+
+	socket.on("chat message", function(data) {
+		// if it's from me
+		if(data.from === $scope.me.socketId) {
+			$scope.converstions[data.to].push(data);
+		}
+		// if it's to me
+		else if(data.to === $scope.me.socketId) {
+			$scope.converstions[data.from].push(data);
+		}
+	});
+
+	socket.on("new user", function(user) {
+		if(user.name != $scope.myName) {
+			$scope.users.push(user);
+			$scope.converstions[user.socketId] = [];
+		}
+		else {
+			$scope.me = {
+				name: $scope.myName,
+				socketId: user.socketId
+			}
+		}
+	});
+
+	socket.on("user left", function(user) {
+		if($scope.converstions[user.socketId]) {
+			$scope.converstions[user.socketId] = [];
+		}
+		var index = $scope.users.indexOf(user);
+		$scope.users.splice(index, 1);
+	});
+
+}]);
+
+app.factory('socket', ['$rootScope', function ($rootScope) {
+  var socket = io.connect();
+
+  return {
+    on: function (eventName, callback) {
+      function wrapper() {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      }
+
+      socket.on(eventName, wrapper);
+
+      return function () {
+        socket.removeListener(eventName, wrapper);
+      };
+    },
+
+    emit: function (eventName, data, callback) {
+      socket.emit(eventName, data, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if(callback) {
+            callback.apply(socket, args);
+          }
+        });
+      });
+    }
+  };
+}]);
+
---
+++ src/server/index.js
@@ -0,0 +1,52 @@
+var express = require("express");
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require("path");
+
+app.use(express.static(__dirname + "/../client/"));
+
+var users = [];
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname + "/../client/templates/index.html"));
+});
+
+app.get('/users', function(req, res){
+  res.send(users);
+});
+
+io.on("connection", function(socket) {
+	//io.emit("user joined", socket.id);
+
+	socket.on("new user", function(name) {
+		var user = {
+			name: name,
+			socketId: socket.id
+		}
+		users.push(user);
+		io.emit("new user", user);
+	})
+
+	//console.log("a user connected");
+	socket.on("disconnect", function() {
+		var user = {};
+		for(var i=0; i<users.length; i++) {
+			if(users[i].socketId === socket.id) {
+				user = users[i];
+				users.splice(i, 1);
+				break;
+			}
+		}
+		io.emit("user left", user);
+	});
+
+	socket.on("chat message", function(msg) {
+		io.emit("chat message", msg);
+	});
+
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
